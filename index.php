5a238de3de70ea2d425d76355d1e9ae619958d26
Aldair <aldier100@gmail.com>
2019-09-18 11:59:41 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-page">
+        <div class="form" >
+          <form action="validar.php" class="login-form">
+            <input type="text" placeholder="usuario" name="usuario"/>
+            <input type="password" placeholder="password" name="password"/>
+            <button >login</button>
+            
+          </form>
+        </div>
+      </div>
+</body>
+</html>
